ab087304a1a952da08af081f9fe2bd764831b2d3
邱国辉 <530784837@qq.com>
2016-12-20 10:49:58 +0800

    项目初始化

--- beginning/server.js
+++
@@ -1,8 +0,0 @@
-// 加载http模块
-var http = require('http')
-// 创建一个服务
-http.createServer(function (req, res) { // 定义接收到请求都回调函数 这个回调函数接收两个参数  分别是req请求体和res响应体
-  res.writeHead(200, {'content-Type': 'text/plain'})
-  res.end('hello World\n')
-}).listen(1337, '127.0.0.1')
-console.log('Server running at http://127.0.0.1:1337/')
